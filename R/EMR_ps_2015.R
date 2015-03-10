1f3f13235898b5689ce44d4f240f168e3b406d3f
Huade <hh561@georgetown.edu>
2015-03-10 12:41:45 -0500

    more coding

--- R/EMR_ps_2015.R
+++ R/EMR_ps_2015.R
@@ -1,5 +1,5 @@
 # R script for thesis
-# Title:
+# Title: 
 # Author: Huade Huo
 # Date: 2015/04
 
@@ -102,19 +102,40 @@ save(physician_cc,file="Data/physician_cc_w_psw.Rda")
 load("Data/physician_cc_w_psw.Rda")
 
 mnps_balance <- bal.table(physician.ps.mnps)
+plot(physician.ps.mnps, plots=3)
+bal.table(physician.ps.mnps, collapse.to = 'covariate', digits = 4)
 
 
+# mnps model
 design.mnps <- svydesign(ids=~1, weights=~psweight, data=physician_cc)
 glm_HealthEdu_pct_mnps <- 
-    svyglm(HealthEdu_pct ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC),
+    svyglm(HealthEdu_pct ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC)+factor(VYEAR),
            design=design.mnps)
 glm_TIMEMD_mnps <- 
-    svyglm(TIMEMD ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC), 
+    svyglm(TIMEMD ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC)+factor(VYEAR), 
            design=design.mnps)
 glm_RETAPPT_pct_mnps <- 
-    svyglm(RETAPPT_pct ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC),
+    svyglm(RETAPPT_pct ~ factor(EMEDREC)+SOLO+factor(OWNS)+factor(MANCAREC)+factor(VYEAR),
            design=design.mnps)
 
+# Test regression assumptions
+pdf("Figures/glm_HealthEdu_pct_mnps.pdf")
+par(mfrow=c(2,2), oma=c(0,0,2,0))
+plot(glm_HealthEdu_pct_mnps,sub.caption="")
+title("Diagnostic plots for the mnps weighted regression of \nHealth Education Prescription on the EHR adoption", outer=T)
+dev.off()
+
+pdf("Figures/glm_TIMEMD_mnps.pdf")
+par(mfrow=c(2,2), oma=c(0,0,2,0))
+plot(glm_TIMEMD_mnps,sub.caption="")
+title("Diagnostic plots for the mnps weighted regression of \nTime Spent with MD on the EHR adoption", outer=T)
+dev.off()
+
+pdf("Figures/glm_RETAPPT_pct_mnps.pdf")
+par(mfrow=c(2,2), oma=c(0,0,2,0))
+plot(glm_RETAPPT_pct_mnps,sub.caption="")
+title("Diagnostic plots for the mnps weighted regression of \nReturned Appt. Rate on the EHR adoption", outer=T)
+dev.off()
 
 # Check distribution of outcome variables
 ggplot(physician_cc, 
@@ -187,7 +208,7 @@ physician_cc_full_EMR$FullEMR <- ifelse(physician_cc_full_EMR$EMEDREC==1,1,0)
 physician_cc_part_EMR <- physician_cc[physician_cc$EMEDREC!=1,]
 physician_cc_part_EMR$PartEMR <- ifelse(physician_cc_part_EMR$EMEDREC==2,1,0)
 
-### Estimate propensity score with GBM
+### Estimate propensity score with GBM (separate model)
 physician.ps.full <- ps(FullEMR ~ OWNS + MSA + MANCAREC + SPECR+ SOLO+ 
                             REGION  + TOTCHRON_mean + Avg_Patient_Age + 
                             PAYPRIV_pct + PAYMCARE_pct + PAYMCAID_pct + PAYWKCMP_pct + 
@@ -204,32 +225,7 @@ physician.ps.part <- ps(PartEMR ~ OWNS + MSA + MANCAREC + SPECR+ SOLO+
                         sampw = physician_cc_part_EMR$PHYSWT,
                         verbose =F)
 
-### Access balance
-bal.table(physician.ps.full)
-bal.table(physician.ps.part)
 
-summary(physician.ps.full)
-### Graphic check for fully adoption
-plot.full.1 <- plot(physician.ps.full,plot=1)
-plot.full.2 <- plot(physician.ps.full,plot=2)
-plot.full.3 <- plot(physician.ps.full,plot=3)
-plot.full.4 <- plot(physician.ps.full,plot=4)
-plot.full.5 <- plot(physician.ps.full,plot=5)
-plot.full.6 <- plot(physician.ps.full,plot=6)
-
-grid.arrange(plot.full.1,plot.full.2,plot.full.3,
-             plot.full.4,plot.full.5,plot.full.6, ncol=2)
-
-### Graphic check for partially adoption
-plot.part.1 <- plot(physician.ps.part, plot=1)
-plot.part.2 <- plot(physician.ps.part, plot=2)
-plot.part.3 <- plot(physician.ps.part, plot=3)
-plot.part.4 <- plot(physician.ps.part, plot=4)
-plot.part.5 <- plot(physician.ps.part, plot=5)
-plot.part.6 <- plot(physician.ps.part, plot=6)
-
-grid.arrange(plot.part.1,plot.part.2,plot.part.3,
-             plot.part.4,plot.part.5,plot.part.6,ncol=2)
 
 ### Estimate regression model with ps weighting (one treatment)
 
@@ -319,14 +315,14 @@ lm_RETAPPT_pct_matched_p <-
        physician_part_matched)
 
 # Result reporting
-# find -E . -regex '.*results.txt' -exec cat {} + > all_outputs.txt
+# find -E . -regex '.*results.tex' -exec cat {} + > all_outputs.tex
 
 ## Descriptive statistics
 physician_tableNominal <- 
     physician_cc[, c("VYEAR","OWNS","MSA",
                      "MANCAREC","SPECR","REGION","SOLO"), with=F]
 
-desc_des<- file("Outputs/LaTeX/01_desc_des_results.txt",open="wt")
+desc_des<- file("Outputs/01_desc_des_results.tex",open="wt")
 sink(desc_des)
 tableNominal(vars = physician_tableNominal, 
              group = physician_cc$EMEDREC, print.pval = "chi2", 
@@ -342,7 +338,7 @@ physician_tableContinuous <-
                       "VYEAR","OWNS","MSA","MANCAREC","SPECR","REGION","SOLO"), 
                  with=F]
 
-desc_cont <- file("Outputs/LaTeX/02_desc_cont_results.txt",open="wt")
+desc_cont <- file("Outputs/02_desc_cont_results.tex",open="wt")
 sink(desc_cont)
 tableContinuous(vars = physician_tableContinuous,
                 group = physician_cc$EMEDREC, 
@@ -370,7 +366,7 @@ indep_label_mnps <- c("Full EMR",
                       "MCC 3-10",
                       "MCC greater than 10")
 
-mnps_results <- file("Outputs/LaTeX/03_mnps_results.txt",open="wt")
+mnps_results <- file("Outputs/03_mnps_results.tex",open="wt")
 sink(mnps_results)
 stargazer(glm_HealthEdu_pct_mnps,glm_TIMEMD_mnps,glm_RETAPPT_pct_mnps,
           title="Estimated effect of EMR adoption with multinomial 
@@ -382,12 +378,41 @@ stargazer(glm_HealthEdu_pct_mnps,glm_TIMEMD_mnps,glm_RETAPPT_pct_mnps,
           label = "tab:mnps")
 close(mnps_results)
 
+## mnps result
+dep_label_mnps <- c("Health Education (Binomial)","Time Spent with MD (Poisson)",
+                    "Returned Appointment Rate (Binomial)")
+indep_label_mnps <- c("Full EMR",
+                      "Partial EMR",
+                      "SOLO",
+                      "HMO",
+                      "Community health center",
+                      "Medical/academic health center",
+                      "Other hospital",
+                      "Other health care corporation",
+                      "Other owner type",
+                      "MCC less than 3",
+                      "MCC 3-10",
+                      "MCC greater than 10")
+
+mnps_results_other_dist <- file("Outputs/04_mnps_other_dist.tex",open="wt")
+sink(mnps_results_other_dist)
+stargazer(glm_HealthEdu_pct_mnps_binomial, glm_TIMEMD_mnps_poisson, 
+          glm_RETAPPT_pct_mnps_binomial,
+          title="Estimated effect of EMR adoption with multinomial 
+          propensity score weighted GLM models",align=T,
+          dep.var.labels=dep_label_mnps,
+          covariate.labels = indep_label_mnps,
+          no.space = T,
+          font.size = "footnotesize",
+          label = "tab:mnps_glm")
+close(mnps_results_other_dist)
+
 ## mnps with no covariates
 dep_label_mnps_nocov <- c("Health Education","Time Spent with MD",
                           "Returned Appointment Rate")
 indep_label_mnps_nocov <- c("Full EMR","Partial EMR")
 
-mnps_no_cov <- file("Outputs/LaTeX/04_mnps_no_cov_results.txt",open="wt")
+mnps_no_cov <- file("Outputs/05_mnps_no_cov_results.tex",open="wt")
 sink(mnps_no_cov)
 stargazer(glm_HealthEdu_pct_mnps_nocontrol,glm_TIMEMD_mnps_nocontrol,
           glm_RETAPPT_pct_mnps_nocontrol,
@@ -443,7 +468,7 @@ indep_label_mnps_allcov <- c("Full EMR",
                              "2009",
                              "2010")
 
-mnps_all_cov <- file("Outputs/LaTeX/05_mnps_all_cov_results.txt",open="wt")
+mnps_all_cov <- file("Outputs/06_mnps_all_cov_results.tex",open="wt")
 sink(mnps_all_cov)
 stargazer(glm_HealthEdu_pct_mnps_allcontrols,glm_TIMEMD_mnps_allcontrols,
           glm_RETAPPT_pct_mnps_allcontrols,
@@ -483,7 +508,7 @@ indep_label_sep_part <- c("Partial EMR",
                           "MCC 3-10",
                           "MCC greater than 10")
 
-ps_sep_full <- file("Outputs/LaTeX/06_ps_sep_full_results.txt",open="wt")
+ps_sep_full <- file("Outputs/07_ps_sep_full_results.tex",open="wt")
 sink(ps_sep_full)
 stargazer(glm_HealthEdu_pct_full,glm_TIMEMD_full,
           glm_RETAPPT_pct_full,
@@ -497,7 +522,7 @@ stargazer(glm_HealthEdu_pct_full,glm_TIMEMD_full,
           label = "tab:ps.sep.full")
 close(ps_sep_full)
 
-ps_sep_part <- file("Outputs/LaTeX/07_ps_sep_part_results.txt",open="wt")
+ps_sep_part <- file("Outputs/08_ps_sep_part_results.tex",open="wt")
 sink(ps_sep_part)
 stargazer(glm_HealthEdu_pct_part,glm_TIMEMD_part,
           glm_RETAPPT_pct_part,
@@ -538,7 +563,7 @@ indep_label_psm_part <- c("Partial EMR",
                           "MCC 3-10",
                           "MCC greater than 10")
 
-ps_psm_full <- file("Outputs/LaTeX/08_ps_psm_full_results.txt",open="wt")
+ps_psm_full <- file("Outputs/09_ps_psm_full_results.tex",open="wt")
 sink(ps_psm_full)
 stargazer(lm_HealthEdu_pct_matched_f,lm_TIMEMD_matched_f,
           lm_RETAPPT_pct_matched_f,
@@ -551,7 +576,7 @@ stargazer(lm_HealthEdu_pct_matched_f,lm_TIMEMD_matched_f,
           label = "tab:ps.psm.full")
 close(ps_psm_full)
 
-ps_psm_part <- file("Outputs/LaTeX/09_ps_psm_part_results.txt",open="wt")
+ps_psm_part <- file("Outputs/10_ps_psm_part_results.tex",open="wt")
 sink(ps_psm_part)
 stargazer(lm_HealthEdu_pct_matched_p,lm_TIMEMD_matched_p,
           lm_RETAPPT_pct_matched_p,
